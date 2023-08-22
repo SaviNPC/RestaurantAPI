a57512f9bf07e66513092c85d584b7ee46cb5f54
Santiago <122063233+SaviNPC@users.noreply.github.com>
2023-08-21 22:29:46 -0300

    Cambios a views, urls, serializers. Users.

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -7,11 +7,14 @@ class Category(models.Model):
 	slug = models.SlugField()
 	title = models.CharField(max_length=255, db_index=True)
 
+	def __str__(self) -> str:
+		return self.title
+
 class MenuItem(models.Model):
 	title = models.CharField(max_length=255, db_index=True)
 	price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
 	featured = models.BooleanField(db_index=True)
-	category = models.ForeignKey(Category, on_delete=models.PROTECT)
+	category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
 
 class Cart(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -39,6 +42,3 @@ class OrderItem(models.Model):
 
 	class Meta:
 		unique_together = ('order', 'menuitem')
-		
-class Prueba(models.Model):
-	prueba = models.SmallIntegerField()
--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('menu-items/', views.menu_items),
+    path('menu-items/<int:pk>', views.single_item),
+    
+    # 'cart/menu-items/'
+    # 'orders/'
+    # 'orders/<int:pk>/'
+    # users > users/users/me > token/login
+]
